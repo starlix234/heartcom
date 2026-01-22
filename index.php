edb680d46f850efe1699592b696907f323f47e8a
Alumno Plaza Vespucio <pv-alumno@duoc.acad>
2026-01-22 10:21:52 -0300

    integracion de archivo estilo usuario rol

--- index.php
+++ index.php
@@ -22,11 +22,11 @@ $listaProyectos = (isset($proyecto) && is_array($proyecto)) ? $proyecto : [];
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>HeartCom</title>
-    
+    <link rel="icon" href="assets/img/logo/logo_heartcom">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <link rel="stylesheet" href="assets/css/index.css">
-    <link rel="icon" href="assets/img/logo/logo_heartcom.ico">
+    
 </head>
 
 <body>
